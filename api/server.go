2e5f34bffe5e85fbbd1a3a37d2a17e527db3463c
vvictorl <102740406+vvictorl@users.noreply.github.com>
2022-10-05 11:06:58 +0900

    chore: add unix timestamp to track bot action

--- api/server.go
+++ api/server.go
@@ -4,6 +4,8 @@ import (
 	cfg "github.com/Carina-labs/HAL9000/config"
 	"github.com/Carina-labs/HAL9000/utils"
 	ut "github.com/Carina-labs/HAL9000/utils/types"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"net/http"
 	"sync"
@@ -30,7 +32,21 @@ func init() {
 	BotStatus = &TxCheckPoint{LastCommit: time.Now()}
 }
 
+func Tracker(ch <-chan bool) {
+	g := promauto.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "current_commit_time",
+			Help: "The last time the bot transaction was executed",
+		},
+	)
+	for range ch {
+		g.SetToCurrentTime()
+		time.Sleep(time.Second * 2)
+	}
+}
+
 func (s Server) On(addr string) {
+
 	http.Handle("/metrics", promhttp.Handler())
 	http.Handle("/check/", NewChkHandler())
 	err := http.ListenAndServe(addr, nil)
@@ -39,10 +55,13 @@ func (s Server) On(addr string) {
 
 func OpenMonitoringSrv(wg *sync.WaitGroup, datach <-chan time.Time, flags cfg.MonitorFlag) {
 	defer wg.Done()
+	relay := make(chan bool)
 	go func() {
 		for t := range datach {
 			BotStatus.SetCommitTime(t)
+			relay <- true
 		}
 	}()
+	go Tracker(relay)
 	Server{}.On(flags.GetExtIP())
 }

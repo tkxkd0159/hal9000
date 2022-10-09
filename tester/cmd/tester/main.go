ea94510a0c4dc5520d5ed3825e041b258744bcea
vvictorl <102740406+vvictorl@users.noreply.github.com>
2022-10-09 19:33:33 +0900

    chore: create a basic bot structure for a tester

--- tester/cmd/tester/main.go
+++ tester/cmd/tester/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"log"
+	"os"
 	"sync"
 	"time"
 
+	sdktypes "github.com/cosmos/cosmos-sdk/types"
+
 	"github.com/Carina-labs/HAL9000/api"
+	"github.com/Carina-labs/HAL9000/client/base"
 	"github.com/Carina-labs/HAL9000/client/base/query"
 	basetypes "github.com/Carina-labs/HAL9000/client/base/types"
+	novaTx "github.com/Carina-labs/HAL9000/client/nova/msgs"
 	cfg "github.com/Carina-labs/HAL9000/config"
 	"github.com/Carina-labs/HAL9000/logic"
 	"github.com/Carina-labs/HAL9000/utils"
@@ -16,30 +22,35 @@ const (
 	NumWorker = 2
 )
 
-// TODO: add subcommand for testing
 func main() {
-	flags := cfg.SetFlags(cfg.ActOracle)
+	botType := cfg.CheckBotType(os.Args[1])
+	flags := cfg.SetFlags(botType)
 	bf := flags.GetBase()
-
 	krDir, logDir := cfg.SetInitialDir(bf.Kn, bf.LogLocation)
 	fdLog, fdErr, fdErrExt := cfg.SetAllLogger(logDir, cfg.StdLogFile, cfg.LocalErrlogFile, cfg.ExtRedirectErrlogFile, bf.Disp)
 	defer utils.CloseFds(fdLog, fdErr, fdErrExt)
-	ctx, krInfo, txf := cfg.SetupBotBase(flags, krDir, fdLog)
+	ctx, krInfo, txf, cni := cfg.SetupBotBase(flags, krDir, fdLog, cfg.ControlChain, "bot_addr")
+	log.SetOutput(ctx.Output)
 
 	wg := new(sync.WaitGroup)
 	wg.Add(NumWorker)
 	botch := make(chan time.Time)
 	go api.OpenMonitoringSrv(wg, botch, flags)
 
-	go func() {
-		defer wg.Done()
-		bot := basetypes.NewBot(ctx, txf, krInfo, bf.Period, fdErr, botch)
-		hostZone := cfg.NewHostChainInfo(bf.HostChain)
-		hostZone.Set()
-		cq := query.NewCosmosQueryClient(hostZone.GrpcAddr)
-		defer utils.CloseGrpc(cq.ClientConn)
-		logic.UpdateChainState(cq, bot, hostZone)
-	}()
+	bot := basetypes.NewBot(ctx, txf, krInfo, bf.Period, fdErr, botch)
+	hostZone := cfg.NewHostChainInfo(bf.HostChain)
+	hostZone.Set()
+	hostZone.WithIBCInfo(flags, botType)
+
+	cq := query.NewCosmosQueryClient(hostZone.GrpcAddr)
+	defer utils.CloseGrpc(cq.ClientConn)
+	delegatedToken, height, apphash := logic.OracleInfo(cq, hostZone.Validator)
+
+	bot.Txf = bot.Txf.WithSequence(320)
+	msg1 := novaTx.MakeMsgUpdateChainState(bot.KrInfo.GetAddress(), hostZone.Name, hostZone.Denom, delegatedToken, height, apphash)
+	msgs := []sdktypes.Msg{msg1}
+	_ = base.GenTxByBot(bot, false, msgs...)
+	_ = cni
 
 	wg.Wait()
 }
--- tester/logic/types.go
+++
@@ -1,7 +0,0 @@
-package logic
-
-type IBCInfo struct {
-	ZoneID  string
-	IBCPort string
-	IBCChan string
-}

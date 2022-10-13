7fb6d4b70788f8ad3e28acf4cfb7ce6efe7d904c
vvictorl <102740406+vvictorl@users.noreply.github.com>
2022-10-13 14:37:17 +0900

    chore: show error more detail with trace

--- utils/os.go
+++ utils/os.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"runtime"
 
 	"github.com/Carina-labs/HAL9000/utils/types"
 )
@@ -24,12 +25,12 @@ func CheckErr(err error, moreMsg string, action types.Code) {
 	switch action {
 	case types.EXIT:
 		if err != nil {
-			panic(fmt.Sprintf("%s: \n %v", moreMsg, err))
+			panic(fmt.Sprintf("%s: \n %+v", moreMsg, err))
 
 		}
 	case types.KEEP:
 		if err != nil {
-			log.Printf("%s: \n %v\n", moreMsg, err)
+			log.Printf("%s: \n %+v\n", moreMsg, err)
 		}
 	}
 }
@@ -42,8 +43,14 @@ func LogErrWithFd(fd *os.File, err error, msg string, action types.Code) {
 		}
 	case types.KEEP:
 		if err != nil {
-			l := log.New(fd, "ERROR (check) : ", log.Llongfile|log.LstdFlags)
-			l.Printf("\n %s: %v\n", msg, err)
+			var logger *log.Logger
+			_, f, l, ok := runtime.Caller(1)
+			if ok {
+				logger = log.New(fd, fmt.Sprintf(" ⛈ : %s:%d ", f, l), log.LstdFlags)
+			} else {
+				logger = log.New(fd, " ⛈ : ", log.LstdFlags)
+			}
+			logger.Printf("\n %s: %v\n", msg, err)
 		}
 	}
 }
